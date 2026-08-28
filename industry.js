8a24c62d83557919394b901a00c81cca3e302da3
kishorekumarm7295-ship-it <kishorekumar.m7295@gmail.com>
2026-08-28 15:39:18 +0530

    Create industry.js

---
+++ industry.js
@@ -0,0 +1,17 @@
+console.log(
+"Industry Portal Loaded 🏢"
+);
+
+
+document.querySelectorAll("button")
+.forEach(button=>{
+
+button.onclick=function(){
+
+alert(
+"Action Completed Successfully 🚀"
+);
+
+}
+
+});
