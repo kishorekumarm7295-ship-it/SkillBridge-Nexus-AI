a4d3af9c6db30cc20adb840b732a7e5188790c63
kishorekumarm7295-ship-it <kishorekumar.m7295@gmail.com>
2026-08-28 15:58:46 +0530

    Create student.js

---
+++ js/student.js
@@ -0,0 +1,17 @@
+console.log(
+"Student Dashboard Loaded 🚀"
+);
+
+
+document.querySelectorAll("button")
+.forEach(btn=>{
+
+btn.onclick=function(){
+
+alert(
+"AI Feature Activated 🤖"
+);
+
+}
+
+});
