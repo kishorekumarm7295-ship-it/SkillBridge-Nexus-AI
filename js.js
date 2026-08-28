f7cc2a85e2f31f80decff511600980c05418a313
kishorekumarm7295-ship-it <kishorekumar.m7295@gmail.com>
2026-08-28 15:29:57 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,23 @@
+console.log("SkillBridge Nexus AI Loaded 🚀");
+
+
+document.querySelector(".chatbot")
+.addEventListener("click",()=>{
+
+alert(
+"Hello 👋 I am SkillBot AI. I can help you find skills, internships and career paths."
+);
+
+});
+
+
+document.querySelectorAll("button")
+.forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+console.log("Button clicked");
+
+});
+
+});
