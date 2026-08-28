76d8b07c1abc3cc96b0ed5334cc3f8fed92e3ee2
kishorekumarm7295-ship-it <kishorekumar.m7295@gmail.com>
2026-08-28 16:15:11 +0530

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,33 @@
+
+let students=5000;
+
+let internships=250;
+
+let placement=92;
+
+
+
+console.log(
+
+"Student Count:",
+students
+
+);
+
+
+
+console.log(
+
+"Internships:",
+internships
+
+);
+
+
+
+console.log(
+
+"Placement Rate:",
+placement+"%"
+
+);
